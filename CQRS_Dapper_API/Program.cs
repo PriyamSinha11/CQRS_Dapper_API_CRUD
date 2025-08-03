bcc73af442eb9baf471b047eb960bf3ad44c1167
Priyam <PriyamSinha1@outlook.con>
2025-08-03 22:09:40 +0530

    CORS POLICY

--- CQRS_Dapper_API/Program.cs
+++ CQRS_Dapper_API/Program.cs
@@ -3,6 +3,17 @@ using CQRS_Dapper_API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // Angular app URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowAngularApp");
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
